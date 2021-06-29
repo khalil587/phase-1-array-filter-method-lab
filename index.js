dea837357258f77455ef4ddcecb94456b12fa401
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-06-29 18:17:47 -0400

    commit tree f9ba40f85a0db8032cbc77bd682e547808ae8f15

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(collection, drivers){
+    return collection.filter( possibleMatch => possibleMatch.toLowerCase() === drivers.toLowerCase ()
+    )
+}
+function fuzzyMatch(collection, drivers) {
+    return collection.filter( tunaFight=>
+      tunaFight.toLowerCase().indexOf(drivers.toLowerCase()) === 0
+    )
+  }
+
+function matchName(name, drivers){
+    return name.filter(possibleMatch => possibleMatch.name === drivers);
+}
